9eb00fa40e63654ce225ceacb796b02118447cf0
Sherin-Denzil <nysherin.denzil@gmail.com>
2020-08-01 23:49:11 -0400

    pushing

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -5,7 +5,7 @@ import { Quiz } from '../quiz.model';
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css'],
+  styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent implements OnInit {
   quiz: Quiz[];
